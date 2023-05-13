0cf13c831a665bdbc932893bc1bb8ca4e83d00bd
Toan <84128650+toan2013dn@users.noreply.github.com>
2023-05-13 16:58:15 +0700

    feat: side nav UI (#6)
    * feat: nav UI
    
    * feat: set default nav button

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Landing from './pages/Landing'
+import TestNav from './pages/TestNav'
 
 const Guard = () => {
   return (
     <Routes>
       <Route element={<Landing />} path="/" />
+      <Route element={<TestNav />} path="/testnav" />
     </Routes>
   )
 }
