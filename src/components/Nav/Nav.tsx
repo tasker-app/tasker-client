0543bef1eb4cd56c1a84816a4941a51f62a9b26c
Toan <84128650+toan2013dn@users.noreply.github.com>
2023-05-15 00:57:51 +0700

    feat: toggle menu nav (#9)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import { ReactComponent as AddIcon } from '@/assets/icons/add.svg'
@@ -8,20 +8,23 @@ import { ReactComponent as DateIcon } from '@/assets/icons/date.svg'
 import { ReactComponent as ExpandIcon } from '@/assets/icons/expand.svg'
 import { Text } from '@/components/Common'
 
-const NavContainer = styled.nav`
-  width: 300px;
+const NavContainer = styled.nav<{ isNavOpen: boolean }>`
+  width: ${(props) => (props.isNavOpen ? '300px' : '0')};
   height: 100vh;
   background-color: #fafafa;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
   border-radius: 8px;
-  padding: 35px 15px;
-  margin-left: 40px;
+  margin: 24px 0 0 40px;
+  transition: width 0.2s ease-in-out;
 `
 
-const NavOptions = styled.div`
-  display: flex;
+const NavOptions = styled.div<{ isNavOpen?: boolean; isOptionDisplayed?: boolean }>`
+  opacity: ${(props) => (props.isNavOpen ? '1' : '0')};
+  display: ${(props) => (props.isOptionDisplayed ? 'flex' : 'none')};
+  transition: opacity 0.2s ease-in-out;
   flex-direction: column;
   gap: 15px;
+  padding: 35px 15px;
 `
 
 const NavButton = styled.button<{ isActive?: boolean }>`
@@ -54,15 +57,16 @@ const Flex = styled.div`
   gap: 8px;
 `
 
-const YourTeams = styled.div`
-  display: flex;
+const YourTeams = styled.div<{ isNavOpen?: boolean; isOptionDisplayed?: boolean }>`
+  display: ${(props) => (props.isOptionDisplayed ? 'flex' : 'none')};
+  opacity: ${(props) => (props.isNavOpen ? '1' : '0')};
   justify-content: space-between;
   align-items: center;
   border-radius: 8px;
-  margin-top: 30px;
-  transition: background-color 0.2s ease-in-out;
-  padding: 4px 5px 4px 10px;
+  margin: 30px 15px 0 15px;
+  transition: background-color, opacity 0.2s ease-in-out;
   cursor: pointer;
+  padding: 5px 5px;
 
   &:hover {
     background-color: #e1e1e1;
@@ -86,16 +90,31 @@ const YourTeams = styled.div`
   }
 `
 
-export const Nav = () => {
-  const [active, setActive] = React.useState('Today')
+type NavProps = {
+  isNavOpen: boolean
+}
+
+export const Nav = ({ isNavOpen }: NavProps) => {
+  const [active, setActive] = useState('Today')
+  const [isOptionDisplayed, setIsOptionDisplayed] = useState(false)
+
+  useEffect(() => {
+    if (isNavOpen) {
+      setTimeout(() => {
+        setIsOptionDisplayed(true)
+      }, 200)
+    } else {
+      setIsOptionDisplayed(false)
+    }
+  }, [isNavOpen])
 
   const handleActive = (value: string) => {
     setActive(value)
   }
 
   return (
-    <NavContainer>
-      <NavOptions>
+    <NavContainer isNavOpen={isNavOpen}>
+      <NavOptions isNavOpen={isNavOpen} isOptionDisplayed={isOptionDisplayed}>
         <NavButton isActive={active === 'Today'} onClick={() => handleActive('Today')}>
           <Flex>
             <CalendarIcon />
@@ -122,7 +141,7 @@ export const Nav = () => {
         </NavButton>
       </NavOptions>
 
-      <YourTeams>
+      <YourTeams isNavOpen={isNavOpen} isOptionDisplayed={isOptionDisplayed}>
         <Text size={16}>Your Teams</Text>
         <div className="FlexIcons">
           <AddIcon />
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,20 +1,18 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import { Header } from '@/components/Header'
+import { Nav } from '@/components/Nav'
 
 const LandingContainer = styled.div``
 
 const Landing = () => {
+  const [isNavOpen, setIsNavOpen] = useState(true)
+
   return (
-    // <LandingContainer>
-    //   <Text color="#000" size={12} type="bold">
-    //     Test
-    //   </Text>
-    //   <ChartIcon />
-    //   <Button />
-    // </LandingContainer>
     <LandingContainer>
-      <Header />
+      <Header handleNavToggle={() => setIsNavOpen(!isNavOpen)} />
+      <Nav isNavOpen={isNavOpen} />
     </LandingContainer>
   )
 }
