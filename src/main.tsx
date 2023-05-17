18286425911314b36d2d73a28cdc232e4feabb81
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-05-17 16:44:39 +0700

    feat: date picker component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import './styles/reset.css'
 import './styles/styles.css'
+import 'air-datepicker/air-datepicker.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Dashboard from './pages/Dashboard'
+import DatePage from './pages/DatePage'
 import SignIn from './pages/SignIn'
 
 const Guard = () => {
@@ -13,6 +15,7 @@ const Guard = () => {
     <Routes>
       <Route element={<Dashboard />} path="/" />
       <Route element={<SignIn />} path="/signin" />
+      <Route element={<DatePage />} path="/date" />
     </Routes>
   )
 }
