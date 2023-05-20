4504e3bdc06b31c6658e21079fb0a25721d5250b
Toan <84128650+toan2013dn@users.noreply.github.com>
2023-05-20 16:04:33 +0700

    feat: add task flow (#25)

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -12,13 +12,14 @@ import { PRIORITY_LIST } from '@/libs/constant'
 
 import { SelectPriority } from './SelectPriority'
 
-const AddTaskContainer = styled.div`
+const AddTaskContainer = styled.div<{ isCancelAddTask: boolean }>`
   height: 190px;
   border: 1px solid rgba(148, 148, 148, 0.8);
   border-radius: 12px;
   padding: 15px 40px 18px 27px;
   box-sizing: border-box;
   margin-top: 40px;
+  display: ${(props) => (props.isCancelAddTask ? 'none' : 'block')};
 `
 
 const AddTaskContent = styled.div`
@@ -96,7 +97,7 @@ const ButtonFlex = styled.div`
   gap: 20px;
 `
 
-const Button = styled.button`
+const Button = styled.button<{ isTaskNameEmpty?: boolean }>`
   padding: 8px 12px;
   border: none;
   background-color: #efefef;
@@ -110,7 +111,9 @@ const Button = styled.button`
 
   &:last-child {
     background: linear-gradient(180deg, rgba(119, 179, 223, 0.5) 0%, rgba(66, 208, 227, 0.255) 100%);
-    transition: background-color 0.3s ease-in-out;
+    opacity: ${(props) => (props.isTaskNameEmpty ? 0.5 : 1)};
+    cursor: ${(props) => (props.isTaskNameEmpty ? 'not-allowed' : 'pointer')};
+    transition: opacity 0.3s ease-in-out;
 
     &:hover {
       background: linear-gradient(180deg, rgba(119, 179, 223, 0.8) 0%, rgba(66, 208, 227, 0.5) 100%);
@@ -125,13 +128,46 @@ const MAPPING_FLAG_ICON = {
   high: <RedFlag />
 }
 
-export const AddTask = () => {
+type AddTaskProps = {
+  setAddNewTask: (isAddNewTask: boolean) => void
+  setIsStatusHidden: (isStatusHidden: boolean) => void
+  setTasks: (tasks: any) => void
+  tasks: any
+}
+
+export const AddTask = ({ setAddNewTask, setTasks, tasks, setIsStatusHidden }: AddTaskProps) => {
   const [taskName, setTaskName] = useState('')
   const [taskDescription, setTaskDescription] = useState('')
   const [priority, setPriority] = useState('default')
+  const [dueDate, setDueDate] = useState(0)
+  const [isCancelAddTask, setIsCancelAddTask] = useState(false)
+
+  const handleAddTask = () => {
+    if (isTaskNameEmpty) return null
+
+    const newTask = {
+      id: tasks.length + 1,
+      name: taskName,
+      description: taskDescription,
+      priority: priority,
+      dueDate: dueDate
+    }
+
+    setTasks((prevTasks: any) => [...prevTasks, newTask])
+    setIsCancelAddTask(true)
+    setAddNewTask(false)
+  }
+
+  const isTaskNameEmpty = taskName.trim() === ''
+
+  const handleCancelAddTask = () => {
+    setIsCancelAddTask(true)
+    setAddNewTask(false)
+    setIsStatusHidden(false)
+  }
 
   return (
-    <AddTaskContainer>
+    <AddTaskContainer isCancelAddTask={isCancelAddTask}>
       <AddTaskContent>
         <TaskName placeholder="Task name" type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
         <TaskDescription
@@ -141,7 +177,7 @@ export const AddTask = () => {
           onChange={(e) => setTaskDescription(e.target.value)}
         />
         <TaskProperties>
-          <DatePicker />
+          <DatePicker setDueDate={setDueDate} />
           <SelectPriority list={PRIORITY_LIST} setValue={setPriority} value={priority}>
             <TaskPropertiesButton>
               {MAPPING_FLAG_ICON[priority as keyof typeof MAPPING_FLAG_ICON]}
@@ -159,10 +195,10 @@ export const AddTask = () => {
           </TaskPropertiesButton>
         </TaskProperties>
         <ButtonFlex>
-          <Button>
+          <Button onClick={() => handleCancelAddTask()}>
             <Text>Cancel</Text>
           </Button>
-          <Button>
+          <Button isTaskNameEmpty={isTaskNameEmpty} onClick={handleAddTask}>
             <Text>Add Task</Text>
           </Button>
         </ButtonFlex>
