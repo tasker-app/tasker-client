5053d65cf515eb725e94d9b30abeae4b336ee01c
Toan <84128650+toan2013dn@users.noreply.github.com>
2023-05-30 13:13:41 +0700

    feat: sort and filter at dashboard (#43)
    * feat: sorted and filterd tasks
    
    * perf: useMemo for sort tasks function
    
    * refactor: type for sort, order and priority
    
    ---------
    
    Co-authored-by: tienthinh-fe <thinhhtde150233@fpt.edu.vn>

--- src/models/task.d.ts
+++ src/models/task.d.ts
@@ -1,7 +1,13 @@
+export type Sort = 'default' | 'name' | 'date' | 'priority'
+
+export type Order = 'ascending' | 'descending'
+
+export type Priority = 'default' | 'low' | 'medium' | 'high'
+
 export type Task = {
   id: string
   name: string
   description: string
-  priority: 'low' | 'medium' | 'high' | 'default'
+  priority: Priority
   dueDate: number
 }
